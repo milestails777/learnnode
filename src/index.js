692cbbcb813a5fb069f91056d7bc756b28fc5f8e
Samir Miles Cibis <samir.cibis@tptlive.ee>
2025-11-24 09:54:11 +0200

    1.3

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//console.log("Hello via Bun!");
+
+//const common = require("./common.js");
+//import common from './common.js';
+//common.hello();
+
+//import es from './es.js';
+//es.hello();
+
+//import fs from 'fs';
+
+//fs.writeFileSync('text.txt', 'Hello Samirius File!!!')
+
+//import boxen from 'boxen';
+
+//console.log(boxen('unicorn', {padding: 1}));
