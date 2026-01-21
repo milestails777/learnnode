efe0c9f644cd91b5df9744e4da634d87c0a6a869
Samir Cibis <samir.cibis@tptlive.ee>
2026-01-21 15:45:28 +0200

    add todo 1.5 samir cibis

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
 import { ref } from 'vue';
 
-    let message = ref('Hello vue!');
+let items = ref(['Sai', 'Piim', 'Viin', 'õlu']);
+let newItem = ref('');
+
+function add(){
+    if(newItem.value.trim() !== '') {
+        items.value.push(newItem.value.trim());
+    }
+    newItem.value = '';
+}
 
 //function input(event) {
  //   console.log(event.target.value);
@@ -10,9 +18,25 @@ import { ref } from 'vue';
 </script>
 
 <template>
-    <h1>{{ message.split('').reverse().join('') }}</h1>
-    <button class="button is-primary" @click="message = 'Hello TA24B!'">Click the Button</button>
-    <input class="input" v-model="message">
+    <div class="container">
+        <div class="field has-addons mt-2">
+            <div class="control is-expanded">
+                <input v-model="newItem" class="input" type="text" @keypress.enter="add">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="add">
+                Search
+                </button>
+            </div>
+        </div>
+        <div class="content">
+            <hl>All Items</hl>
+            <ul>
+                <li v-for="item in items">{{item}}</li>
+                <li>Linus Torvalds</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <style>
