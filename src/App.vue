69a0b35305d542f4f3fccb19a34281844416b81a
Samir Cibis <samir.cibis@tptlive.ee>
2026-01-21 15:07:05 +0200

    1.4

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
-    let message = 'Hello vue!';
+import { ref } from 'vue';
+
+    let message = ref('Hello vue!');
+
+function input(event) {
+    console.log(event.target.value);
+}
 </script>
 
 <template>
     <h1>{{ message }}</h1>
-    <button class="button is-primary" v-on:click="console.log('click')">Click the Button</button>
+    <button class="button is-primary" v-on:click="message = 'Hello TA24B!'">Click the Button</button>
+    <input class="input" v-bind:value="message" v-on:input="input">
 </template>
 
 <style>
-    
+
 </style>
