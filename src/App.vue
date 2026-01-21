977a543e16ac04f5e3b486c8aaaa40d828874539
Samir Cibis <samir.cibis@tptlive.ee>
2026-01-21 15:26:52 +0200

    1.4

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,16 @@ import { ref } from 'vue';
 
     let message = ref('Hello vue!');
 
-function input(event) {
-    console.log(event.target.value);
-}
+//function input(event) {
+ //   console.log(event.target.value);
+ //   message.value = event.target.value;
+//}
 </script>
 
 <template>
-    <h1>{{ message }}</h1>
-    <button class="button is-primary" v-on:click="message = 'Hello TA24B!'">Click the Button</button>
-    <input class="input" v-bind:value="message" v-on:input="input">
+    <h1>{{ message.split('').reverse().join('') }}</h1>
+    <button class="button is-primary" @click="message = 'Hello TA24B!'">Click the Button</button>
+    <input class="input" v-model="message">
 </template>
 
 <style>
