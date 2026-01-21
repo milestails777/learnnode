82527ad25cc3236a92b06f28e8bdb16d276669ec
Samir Cibis <samir.cibis@tptlive.ee>
2026-01-21 14:52:31 +0200

    1.3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
-
+    let message = 'Hello vue!';
 </script>
 
 <template>
     <h1>{{ message }}</h1>
+    <button class="button is-primary" v-on:click="console.log('click')">Click the Button</button>
 </template>
+
+<style>
+    
+</style>
