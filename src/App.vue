8b410f460cb238a400dd6d75ce8be60db5942440
Samir Cibis <samir.cibis@tptlive.ee>
2026-01-28 15:21:57 +0200

    Add complx todo list

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+let i = 1;
 
-let items = ref(['Sai', 'Piim', 'Viin', 'õlu']);
+let items = ref([
+    {id: i++, text: 'Sai', isDone: false}, 
+    {id: i++, text: 'Piim', isDone: true}, 
+    {id: i++, text: 'Viin', isDone: false}, 
+    {id: i++, text: 'õlu', isDone: true},
+    {id: i++, text: 'Linus Torvalds', isDone: true},
+ ]);
 let newItem = ref('');
 
+let doneItems = computed(() => items.value.filter(item => item.isDone));
+let toDoItems = computed(() => items.value.filter(item => !item.isDone));
+
+//Z
 function add(){
     if(newItem.value.trim() !== '') {
-        items.value.push(newItem.value.trim());
+        items.value.push({id: i++, text: newItem.value.trim(), isDone: false});
     }
     newItem.value = '';
 }
@@ -32,8 +43,29 @@ function add(){
         <div class="content">
             <hl>All Items</hl>
             <ul>
-                <li v-for="item in items">{{item}}</li>
-                <li>Linus Torvalds</li>
+                <li v-for="item in items" :key="item.id">
+                    {{ item.text }}
+                    <input type="checkbox" v-model="item.isDone">
+                </li>
+             
+            </ul>
+
+            <hl>Done Items</hl>
+            <ul>
+                <li v-for="item in doneItems" :key="item.id">
+                    {{ item.text }}
+                    <input type="checkbox" v-model="item.isDone">
+                </li>
+                
+            </ul>
+
+            <hl>toDo Items</hl>
+            <ul>
+                <li v-for="item in toDoItems" :key="item.id">
+                    {{ item.text }}
+                    <input type="checkbox" v-model="item.isDone">
+                </li>
+                
             </ul>
         </div>
     </div>
