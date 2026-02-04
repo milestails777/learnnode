d9718d77dfdf0b40d5e5c6279b2407362e7937fc
Samir Cibis <samir.cibis@tptlive.ee>
2026-02-04 14:58:04 +0200

    1.9

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { ref } from 'vue';
+import Modal from './components/Modal.vue';
 
+let modal1Active = ref(false);
+let modal2Active = ref(false);
 </script>
 
 <template>
     <div class="container">
-        
+        <button class="button is-primary" @click="modal1Active = true">Open Modal 1</button>
+        <button class="button is-info" @click="modal2Active = true">Open Modal 2</button>
+        <Modal :active="modal1Active" @close="modal1Active = false" url="https://picsum.photos/200/300?r=1"></Modal>
+        <Modal :active="modal2Active" @close="modal2Active = false" url="https://picsum.photos/200/300?r=1"></Modal>
     </div>
 </template>
 
