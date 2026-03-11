5e97db4264d1d5bc06b402251f56fb4a11ea7f07
Samir Cibis <samir.cibis@tptlive.ee>
2026-03-11 13:17:20 +0200

    add api homework that is interesting

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import ToDo from './pages/ToDo.vue';
 import Modals from './pages/Modals.vue';
 import Messages from './pages/Messages.vue';
 import ChuckNorris from './pages/ChuckNorris.vue';
+import CarFacts from './pages/CarFacts.vue';
 
 const routes = [
   { path: '/', component: ToDo, name: 'ToDo' },
   { path: '/modals', component: Modals, name: 'Modals' },
   { path: '/messages', component: Messages, name: 'Messages' },
-  { path: '/chuck', component: ChuckNorris, name: 'Chuck Norris'}
+  { path: '/chuck', component: ChuckNorris, name: 'Chuck Norris'},
+  { path: '/carfacts', component: CarFacts, name: 'Car Facts' }
 ];
 
 export const router = createRouter({
---
+++ src/pages/CarFacts.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+let fact = ref('');
+let res = await fetch('https://catfact.ninja/fact');
+let data = await res.json();
+fact.value = data.fact;
+</script>
+
+<template>
+<div class="content">
+    <h1>{{ fact }}</h1>
+</div>
+</template>
