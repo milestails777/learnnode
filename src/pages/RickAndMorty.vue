bdff707485674bfe4b0b0c5de4e5ee64a0a7dbf4
Samir Cibis <samir.cibis@tptlive.ee>
2026-03-19 03:27:44 +1400

    NT 4.0 Version, which was made by Samir Cibis

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -3,11 +3,36 @@ import axios from 'axios';
 import { ref } from 'vue';
 import CharacterCard from '../components/CharacterCard.vue';
 const res = await axios.get('https://rickandmortyapi.com/api/character');
-let characters = ref()
+let characters = ref([]);
+let info = ref({
+    count: 0,
+    pages: 0,
+    next: null,
+    prev: null,
+});
 characters.value = res.data.results;
-</script>
+info.value = res.data.info;
 
+async function next() {
+    const res = await axios.get(info.value.next);
+    characters.value = res.data.results;
+    info.value = res.data.info;
+}
+
+async function prev() {
+    const res = await axios.get(info.value.prev);
+    characters.value = res.data.results;
+    info.value = res.data.info;
+}
+
+</script>
 <template>
+<div class="is-flex is-justify-content-space-between mb-2">
+    <button class="button is-primary" :disabled="!info.prev" @click="prev">Prev</button>
+    <button class="button is-primary" :disabled="!info.next" @click="next">Next</button>
+</div>
+
+
 <div class="columns is-multiline">
     <div class="column is-3" v-for="character in characters">
         <CharacterCard :character="character"></CharacterCard>
